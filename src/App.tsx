24621fda644ce24180b13f6213ea4bc5b84b1207
Marco Santarcangelo <marco.s@zksystems.io>
2024-11-02 23:51:51 -0300

    fix position

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ export const App: React.FC = () => {
   return (
     <>
       <Canvas
-        camera={{ position: [0, -100, -100], fov: 30 }}
+        camera={{ position: [0, -100, 100], fov: 30 }}
         style={{ background: "#121212" }}
       >
         <Suspense fallback={null}>
--- src/components/NavigationControls.tsx
+++ src/components/NavigationControls.tsx
@@ -34,7 +34,7 @@ export const NavigationControls = ({
         camera.position.z += moveDistance;
         break;
       case "reset":
-        camera.position.set(0, -100, -100);
+        camera.position.set(0, -100, 100);
         camera.lookAt(0, 0, 0);
         break;
       default:
@@ -78,10 +78,10 @@ export const NavigationControls = ({
         </button>
         <div className="depth-buttons">
           <button onClick={() => moveCamera("forward")} aria-label="Zoom In">
-            -
+            +
           </button>
           <button onClick={() => moveCamera("backward")} aria-label="Zoom Out">
-            +
+            -
           </button>
         </div>
       </div>
